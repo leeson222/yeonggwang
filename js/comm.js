dc5fb8530863cba563755972929d273a9dcd658f
이송이 <160142187+leeson222@users.noreply.github.com>
2025-10-28 17:02:51 +0900

    푸터 추가

--- js/comm.js
+++ js/comm.js
@@ -4,5 +4,26 @@ function updateBannerHeightVar() {
   document.documentElement.style.setProperty('--banner-h', `${banner.offsetHeight}px`);
 }
 
-window.addEventListener('load', updateBannerHeightVar);
-window.addEventListener('resize', updateBannerHeightVar);
+function updateFooterHeightVar() {
+  const footer = document.querySelector('#footer');
+  if (!footer) return;
+  document.documentElement.style.setProperty('--footer-h', `${footer.offsetHeight}px`);
+}
+
+function updateBannerFooterHeightVar() {
+  const banner = document.querySelector('.top-banner-wrap');
+  const footer = document.querySelector('#footer');
+  if (!banner || !footer) return;
+
+  const total = banner.offsetHeight + footer.offsetHeight;
+  document.documentElement.style.setProperty('--banner-footer-h', `${total}px`);
+}
+
+function updateAllHeights() {
+  updateBannerHeightVar();
+  updateFooterHeightVar();
+  updateBannerFooterHeightVar();
+}
+
+window.addEventListener('load', updateAllHeights);
+window.addEventListener('resize', updateAllHeights);
